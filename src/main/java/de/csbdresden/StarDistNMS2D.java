91a3702b3c95275bb64e29544aa78dc8ffe5d36b
Uwe Schmidt <mail@uweschmidt.org>
2019-12-19 10:01:20 +0100

    Improve UI and error messages

--- src/main/java/de/csbdresden/StarDistNMS2D.java
+++ src/main/java/de/csbdresden/StarDistNMS2D.java
@@ -4,10 +4,15 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.scijava.ItemIO;
+import org.scijava.ItemVisibility;
 import org.scijava.command.Command;
 import org.scijava.log.LogService;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
+import org.scijava.ui.DialogPrompt.MessageType;
+import org.scijava.ui.UIService;
+import org.scijava.widget.ChoiceWidget;
+import org.scijava.widget.NumberWidget;
 
 import ij.IJ;
 import ij.ImagePlus;
@@ -26,7 +31,7 @@ import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.view.Views;
 
-@Plugin(type = Command.class, menuPath = "Plugins > StarDist > 2D Non-maximum Suppression", label = "StarDist 2D NMS")
+@Plugin(type = Command.class, menuPath = "Plugins > StarDist > Postprocessing > 2D NMS", label = "StarDist 2D NMS")
 public class StarDistNMS2D<T extends RealType<T>> implements Command {
 
     @Parameter(label="Probability/Score Image")
@@ -38,11 +43,19 @@ public class StarDistNMS2D<T extends RealType<T>> implements Command {
     @Parameter(label="Label Image", type=ItemIO.OUTPUT)
     private Img<?> label;
 
-    @Parameter(label="Probability/Score Threshold", stepSize="0.1")
+    @Parameter(label="Probability/Score Threshold", stepSize="0.05", min="0", max="1", style=NumberWidget.SLIDER_STYLE)
     private double probThresh = 0.5;
 
-    @Parameter(label="NMS Overlap Threshold", stepSize="0.1", min="0", max="1")
+    @Parameter(label="Overlap Threshold", stepSize="0.05", min="0", max="1", style=NumberWidget.SLIDER_STYLE)
     private double nmsThresh = 0.4;
+
+    @Parameter(label="Output Type",
+               choices={"ROI Manager","Label Image","Both"},
+               style=ChoiceWidget.RADIO_BUTTON_HORIZONTAL_STYLE)
+    private String outputType = "ROI Manager";
+
+    @Parameter(visibility=ItemVisibility.MESSAGE)
+    private final String advMsg = "<html><u>Advanced</u></html>";
 
     @Parameter(label="Boundary Exclusion", stepSize="1", min="0")
     private int excludeBoundary = 2;
@@ -50,14 +63,11 @@ public class StarDistNMS2D<T extends RealType<T>> implements Command {
     @Parameter(label="Verbose", description="Verbose output")
     private boolean verbose = false;
 
-    @Parameter(label="Output Type", choices={"ROI Manager","Label Image","Both"})
-    private String outputType = "ROI Manager";
-
     @Parameter
     private LogService log;
-    
-//    @Parameter
-//    private UIService uiService;
+
+    @Parameter
+    private UIService ui;
 //
 //    @Parameter
 //    private OpService opService;
@@ -65,27 +75,27 @@ public class StarDistNMS2D<T extends RealType<T>> implements Command {
 //    @Parameter
 //    private CommandService commandService;
 
-    
+
     private boolean exportPointRois = false;
     private boolean exportBboxRois = false;
-    
+
     private RoiManager roiManager = null;
     private ImagePlus labelImage = null;
     private int labelId = 1;
-    
-    
+
+
     @Override
     public void run() {
-        checkInputs();
-        
+        if (!checkInputs()) return;
+
         @SuppressWarnings("unchecked")
         final RandomAccessibleInterval<FloatType> probRAI = (RandomAccessibleInterval<FloatType>) prob.getImgPlus();
         @SuppressWarnings("unchecked")
         final RandomAccessibleInterval<FloatType> distRAI = (RandomAccessibleInterval<FloatType>) dist.getImgPlus();
-                
+
         final long numFrames = prob.getFrames();
         final boolean isTimelapse = numFrames > 1;
-        
+
         if (isTimelapse) {
             for (int t = 0; t < numFrames; t++) {
                 final Candidates polygons = new Candidates(Views.hyperSlice(probRAI,2,t), Views.hyperSlice(distRAI,3,t), probThresh, excludeBoundary, verbose ? log : null);
@@ -101,7 +111,7 @@ public class StarDistNMS2D<T extends RealType<T>> implements Command {
                 log.info(String.format("%d polygon candidates, %d remain after non-maximum suppression\n", polygons.getSorted().size(), polygons.getWinner().size()));
             export(polygons, 0);
         }
-        
+
         if (outputType.equals("Label Image") || outputType.equals("Both")) {
             // IJ.run(labelImage, "glasbey inverted", "");
             label = ImageJFunctions.wrap(labelImage);
@@ -109,40 +119,52 @@ public class StarDistNMS2D<T extends RealType<T>> implements Command {
     }
     
     
-    private void checkInputs() {
-         if (!( (prob.numDimensions() == 2 && prob.axis(0).type() == Axes.X && prob.axis(1).type() == Axes.Y) ||
-                (prob.numDimensions() == 3 && prob.axis(0).type() == Axes.X && prob.axis(1).type() == Axes.Y && prob.axis(2).type() == Axes.TIME) ))
-             throw new IllegalArgumentException("Probability/Score must be a 2D image or timelapse");
-
-         if (!( (dist.numDimensions() == 3 && dist.axis(0).type() == Axes.X && dist.axis(1).type() == Axes.Y && dist.axis(2).type() == Axes.CHANNEL && dist.getChannels() >= 3) ||
-                (dist.numDimensions() == 4 && dist.axis(0).type() == Axes.X && dist.axis(1).type() == Axes.Y && dist.axis(2).type() == Axes.CHANNEL && dist.getChannels() >= 3 && dist.axis(3).type() == Axes.TIME) ))
-             throw new IllegalArgumentException("Distance must be a 2D image or timelapse with at least three channels");
-         
-         if ((prob.numDimensions() + 1) != dist.numDimensions())
-             throw new IllegalArgumentException("Axes of Probability/Score and Distance not compatible");
-         
-         if (prob.getWidth() != dist.getWidth() || prob.getHeight() != dist.getHeight())
-             throw new IllegalArgumentException("Width or height of Probability/Score and Distance differ");
-         
-         if (prob.getFrames() != dist.getFrames())
-             throw new IllegalArgumentException("Number of frames of Probability/Score and Distance differ");
-
-         if (!(0 <= nmsThresh && nmsThresh < 1))
-             throw new IllegalArgumentException("NMS Threshold must be in interval [0,1).");
-         
-         if (!(outputType.equals("ROI Manager") || outputType.equals("Label Image") || outputType.equals("Both")))
-             throw new IllegalArgumentException("Output type must be one of {\"ROI Manager\", \"Label Image\", \"Both\"}.");
-         
-         if (verbose) {
-             log.info(String.format("probThresh = %f\n", probThresh));
-             log.info(String.format("nmsThresh = %f\n", nmsThresh));
-             log.info(String.format("excludeBoundary = %d\n", excludeBoundary));
-             log.info(String.format("verbose = %s\n", verbose));
-             log.info(String.format("outputKind = %s\n", outputType));
-         }
+    private boolean showError(String msg) {
+        ui.showDialog(msg, MessageType.ERROR_MESSAGE);
+        // log.error(msg);
+        return false;
     }
-    
-    
+
+
+    private boolean checkInputs() {
+        if (!( (prob.numDimensions() == 2 && prob.axis(0).type() == Axes.X && prob.axis(1).type() == Axes.Y) ||
+               (prob.numDimensions() == 3 && prob.axis(0).type() == Axes.X && prob.axis(1).type() == Axes.Y && prob.axis(2).type() == Axes.TIME) ))
+            return showError("Probability/Score must be a 2D image or timelapse.");
+
+        if (!( (dist.numDimensions() == 3 && dist.axis(0).type() == Axes.X && dist.axis(1).type() == Axes.Y && dist.axis(2).type() == Axes.CHANNEL && dist.getChannels() >= 3) ||
+               (dist.numDimensions() == 4 && dist.axis(0).type() == Axes.X && dist.axis(1).type() == Axes.Y && dist.axis(2).type() == Axes.CHANNEL && dist.getChannels() >= 3 && dist.axis(3).type() == Axes.TIME) ))
+            return showError("Distance must be a 2D image or timelapse with at least three channels.");
+
+        if ((prob.numDimensions() + 1) != dist.numDimensions())
+            return showError("Axes of Probability/Score and Distance not compatible.");
+
+        if (prob.getWidth() != dist.getWidth() || prob.getHeight() != dist.getHeight())
+            return showError("Width or height of Probability/Score and Distance differ.");
+
+        if (prob.getFrames() != dist.getFrames())
+            return showError("Number of frames of Probability/Score and Distance differ.");
+
+        if (!(0 <= nmsThresh && nmsThresh < 1))
+            return showError("NMS Threshold must be in interval [0,1).");
+
+        if (excludeBoundary < 0)
+            return showError("Boundary Exclusion must be >= 0");
+
+        if (!(outputType.equals("ROI Manager") || outputType.equals("Label Image") || outputType.equals("Both")))
+            return showError("Output Type must be one of {\"ROI Manager\", \"Label Image\", \"Both\"}.");
+
+        if (verbose) {
+            log.info(String.format("probThresh = %f\n", probThresh));
+            log.info(String.format("nmsThresh = %f\n", nmsThresh));
+            log.info(String.format("excludeBoundary = %d\n", excludeBoundary));
+            log.info(String.format("verbose = %s\n", verbose));
+            log.info(String.format("outputType = %s\n", outputType));
+        }
+        
+        return true;
+    }
+
+
     private void export(Candidates polygons, int framePosition) {
         switch (outputType) {
         case "ROI Manager":
@@ -156,11 +178,11 @@ public class StarDistNMS2D<T extends RealType<T>> implements Command {
             exportLabelImage(polygons, framePosition);
             break;
         default:
-            log.error(String.format("Unknown output type \"%s\"", outputType));
+            showError(String.format("Unknown output type \"%s\"", outputType));
         }
     }
-    
-    
+
+
     private void exportROIs(Candidates polygons, int framePosition) {
         if (roiManager == null) {
             IJ.run("ROI Manager...", "");
@@ -186,16 +208,16 @@ public class StarDistNMS2D<T extends RealType<T>> implements Command {
                 if (framePosition > 0) bboxRoi.setPosition(framePosition);
                 roiManager.addRoi(bboxRoi);
             }
-        }            
+        }
     }
-    
-    
+
+
     private void exportLabelImage(Candidates polygons, int framePosition) {
         if (labelImage == null)
             labelImage = IJ.createImage("Labeling", "16-bit black", (int)prob.getWidth(), (int)prob.getHeight(), 1, 1, (int)prob.getFrames());
         if (prob.getFrames() > 1)
             labelImage.setT(framePosition);
-        final ImageProcessor ip = labelImage.getProcessor();        
+        final ImageProcessor ip = labelImage.getProcessor();
         final List<Integer> winner = polygons.getWinner();
         final int numWinners = winner.size();
         // winners are ordered by score -> draw from last to first to give priority to higher scores in case of overlaps
@@ -206,15 +228,15 @@ public class StarDistNMS2D<T extends RealType<T>> implements Command {
         }
         labelId += numWinners;
     }
-    
+
 
     public static void main(final String... args) throws Exception {
         final ImageJ ij = new ImageJ();
         ij.launch(args);
-        
+
         Dataset prob = ij.scifio().datasetIO().open(StarDistNMS2D.class.getClassLoader().getResource("blobs_prob.tif").getFile());
         Dataset dist = ij.scifio().datasetIO().open(StarDistNMS2D.class.getClassLoader().getResource("blobs_dist.tif").getFile());
-        
+
         ij.ui().show(prob);
         ij.ui().show(dist);
 
